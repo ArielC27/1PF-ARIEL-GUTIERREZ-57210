b8f771469f947dbd007aea8f28a5a1ad2576344f
ArielC27 <109997773+ArielC27@users.noreply.github.com>
2024-08-07 20:30:38 -0300

    Remove store to app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,16 +7,9 @@ import { provideAnimationsAsync } from "@angular/platform-browser/animations/asy
 import { provideNativeDateAdapter } from "@angular/material/core";
 import { MAT_FORM_FIELD_DEFAULT_OPTIONS } from "@angular/material/form-field";
 import { provideHttpClient, withFetch } from "@angular/common/http";
-import { StoreModule } from "@ngrx/store";
-import { rootReducer } from "./core/store/store";
-
 @NgModule({
   declarations: [AppComponent],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    StoreModule.forRoot(rootReducer, {}),
-  ],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [
     provideAnimationsAsync(),
     provideNativeDateAdapter(),

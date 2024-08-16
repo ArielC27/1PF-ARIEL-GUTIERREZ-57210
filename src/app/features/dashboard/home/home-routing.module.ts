e6edfde92584a287c0c2b6bf7dea83ae7de6548f
Ariel Gutierrez <agutierrez@gmssa.com.ar>
2024-08-16 11:46:27 -0300

    Added icon home

--- src/app/features/dashboard/home/home-routing.module.ts
+++ src/app/features/dashboard/home/home-routing.module.ts
@@ -1,11 +1,28 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
-import { HomeComponent } from './home.component';
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
+import { HomeComponent } from "./home.component";
 
 const routes: Routes = [
   {
-    path: '',
+    path: "",
     component: HomeComponent,
+  },
+  {
+    path: "courses",
+    loadChildren: () =>
+      import("../courses/courses.module").then((m) => m.CoursesModule),
+  },
+  {
+    path: "inscriptions",
+    loadChildren: () =>
+      import("../inscripciones/inscripciones.module").then(
+        (m) => m.InscripcionesModule
+      ),
+  },
+  {
+    path: "students",
+    loadChildren: () =>
+      import("../students/students.module").then((m) => m.StudentsModule),
   },
 ];
 
